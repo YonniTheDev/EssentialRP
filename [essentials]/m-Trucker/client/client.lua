db64ab72fcaeb9991ca70db116dbe0352049dbbc
YonniTheDev <142137367+YonniTheDev@users.noreply.github.com>
2023-08-13 08:47:57 -0400

    Screenshot changes

--- [essentials]/m-Trucker/client/client.lua
+++
@@ -1,3 +0,0 @@
-RegisterNetEvent("m-Trucker:client:Waypoint", function()
-    SetNewWaypoint(1204.5278320313, -3103.162109375)  
-end)
--- [essentials]/nc-loadingscreenv3/html/js/cursor.js
+++
@@ -1,7 +0,0 @@
-function updateColors()
-{
-    // cursor
-    document.documentElement.style.setProperty("--cursor-dot", config.BASE.color.cursor_dot);
-    document.documentElement.style.setProperty("--cursor-ring", config.BASE.color.cursor_ring);
-
-}
