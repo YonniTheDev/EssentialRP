96474146419fb7c30bc68835e3092b838a9a1e1a
YonniTheDev <142137367+YonniTheDev@users.noreply.github.com>
2023-08-13 23:51:03 -0400

    Civ Clothes Pck 1

---
+++ [Clothes]/CivClothes/fxmanifest.lua
@@ -0,0 +1,20 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_f_freemode_01_mp_f_femalejbib.meta',
+  'mp_m_freemode_01_mp_m_civclothes.meta',
+  'mp_f_freemode_01_mp_f_civclothes.meta',
+  'mp_m_freemode_01_mp_m_malejbib2.meta',
+  'mp_m_freemode_01_mp_m_malejbib.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_femalejbib.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_civclothes.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_civclothes.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_malejbib2.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_malejbib.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
