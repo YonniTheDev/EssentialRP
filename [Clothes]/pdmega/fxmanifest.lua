ea0d4f09c2944975f3e7f4f8cf9efb1d28cf884f
YonniTheDev <142137367+YonniTheDev@users.noreply.github.com>
2023-08-14 00:20:15 -0400

    PD Clothing

---
+++ [Clothes]/pdmega/fxmanifest.lua
@@ -0,0 +1,18 @@
+-- Generated with DurtyClothTool
+
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_pdmegapack.meta',
+  'mp_f_freemode_01_mp_f_pdmegapack.meta',
+  'mp_m_freemode_01_mp_m_balacalava.meta',
+  'mp_f_freemode_01_mp_f_balacalava.meta',
+  'stream/pedalternatevariations.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_pdmegapack.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_pdmegapack.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_balacalava.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_balacalava.meta'
+data_file 'ALTERNATE_VARIATIONS_FILE' 'stream/pedalternatevariations.meta'
