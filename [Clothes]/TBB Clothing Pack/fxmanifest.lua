08655177160c14aef01ca1b94523ce13c7464eeb
YonniTheDev <142137367+YonniTheDev@users.noreply.github.com>
2023-08-13 21:56:14 -0400

    Final Civ Push

---
+++ [Clothes]/TBB Clothing Pack/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+files {
+    'mp_f_freemode_01_tbb.meta',
+    'mp_m_freemode_01_tbb.meta'
+ 
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_tbb.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_tbb.meta'
+
